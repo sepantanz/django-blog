331b7b8a028e1aa4dd3a0259550be4f675e2a41d
sepantanz <35305466+sepantanz@users.noreply.github.com>
2023-10-04 20:57:50 +0330

    registration and login functionality implemented

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from assignments.models import About
-
+from blog_main.forms import RegistrationForm
 from blogs.models import Blog, Category
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 def home(request):
@@ -22,3 +24,39 @@ def home(request):
         'about': about
     }
     return render(request, 'home.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    else:
+        form = RegistrationForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'register.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect('home')
+    form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
